acbd33e2a4d6dd439c2adee852c1c967c9dd84b1
Victor Talamantes <victor@binmatter.com>
2022-08-18 18:48:47 -0600

    Add editor area

--- src/App.svelte
+++ src/App.svelte
@@ -4,7 +4,11 @@
 <Topbar />
 <div id="wrapper">
    <div id="flowchart-area"></div>
-   <div id="text-area"></div>
+   <div id="text-area">
+      <div class="editor" contenteditable="true" spellcheck="false">
+         hola
+      </div>
+   </div>
 </div>
 <style lang="scss">
    @import "./styles/variables.scss";
@@ -17,6 +21,15 @@
       #text-area {
          width: 70%;
          background-color: $editor-background;
+         height: calc(100vh - $topbar-height);
+         overflow: scroll;
+
+         .editor {
+            outline: 0px solid transparent;
+            min-height: calc(100% - 2rem);
+            color: white;
+            padding: 1rem;
+         }
       }
 
       #flowchart-area {
