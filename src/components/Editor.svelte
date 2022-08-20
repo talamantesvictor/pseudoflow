39a2636e71962cd1ec0eee67ed2663f8de4326e1
Victor Talamantes <victor@binmatter.com>
2022-08-20 17:31:43 -0600

    Separate editor on its own component

---
+++ src/components/Editor.svelte
@@ -0,0 +1,64 @@
+<script lang="ts">
+   import { onMount } from "svelte";
+
+   let editorHeight: number;
+   $: drawLineNumbers(editorHeight / 24); // scss variable $line-height = 24
+
+   function drawLineNumbers(rows) {
+      if (rows > 0) {
+         let rowsHtml: string = '';
+         for (let index = 0; index < rows; index++) {
+            rowsHtml += index + 1 + '<br>';
+         }
+         document.getElementsByClassName('line-numbers')[0].innerHTML = rowsHtml;
+      }
+   }
+   function keyDownController(e) {
+      if (e.key === "Tab") {
+         e.preventDefault();
+      }
+   }
+   function focusOnEditableArea() {
+      document.getElementById('editor-editable-area').focus();
+   }
+
+   onMount(() => {
+      focusOnEditableArea();
+   });
+</script>
+
+<div class="numbers-area">
+   <div class="line-numbers"></div>
+</div>
+<div class="editor-area" on:click="{focusOnEditableArea}">
+   <div bind:clientHeight="{editorHeight}">
+      <div id="editor-editable-area" contenteditable="true" spellcheck="false" on:keydown="{keyDownController}"></div>
+   </div>
+</div>
+
+<style lang="scss">
+   @import "../styles/variables.scss";
+
+   .numbers-area {
+      padding: 1rem 1rem 1rem 0.5rem;
+      .line-numbers {
+         color: $linenumbers-foreground;
+         background-color: $linenumbers-background;
+         min-width: 36px;
+         text-align: right;
+      }
+   }
+
+   .editor-area {
+      width: 100%;
+      flex-grow: 1;
+      min-height: calc(100% - 2rem);
+      color: white;
+      padding: 1rem 1rem 1rem 0rem;
+
+      [contenteditable] {
+         outline: 0px solid transparent;
+         pointer-events: all;
+      }
+   }
+</style>
