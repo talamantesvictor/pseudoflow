f119f9bef28f59ec1290debb7cca4b1e3577ac18
Victor Talamantes <victor@binmatter.com>
2022-09-26 16:03:32 -0600

    Remove comments from pseudocode when tokenizing

--- src/lib/editor/analyzers.ts
+++ src/lib/editor/analyzers.ts
@@ -1,5 +1,9 @@
 export const tokenize = (code: string) => {
+   // remove comments
+   code = code.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'');
+   // separate values to tokenize
    let regex = /(["'])(?:(?=(\\?))\2.)*?\1|(?:[\\(){}[\]=&|^+<>/*%;.\:?!~-]{1,2}|(?:\w+|\d+))/g;
    let tokens = code.match(regex);
+
    return tokens;
 }
