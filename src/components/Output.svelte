46a34079f407819c5b2ceafe2a385a78f0d96079
Victor Talamantes <victor@binmatter.com>
2022-10-08 15:05:20 -0600

    Add basic output component

---
+++ src/components/Output.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+   export let content: string;
+</script>
+<div id="output-content">
+   {@html content}
+</div>
+<style lang="scss">
+   #output-content {
+      padding: 1rem;
+      color: white;
+   }
+</style>
