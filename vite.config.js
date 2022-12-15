9bd1872e4630fd7fa2b10629156a39c09d60b1d7
Victor Talamantes <victor@binmatter.com>
2022-12-15 16:29:31 -0700

    Add tauri configuration files to generate app binaries

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,24 @@
-import { defineConfig } from 'vite';
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import sveltePreprocess from 'svelte-preprocess';
+import { defineConfig } from "vite";
+import { svelte } from "@sveltejs/vite-plugin-svelte";
+import sveltePreprocess from "svelte-preprocess";
 
 export default defineConfig({
-   plugins: [svelte({
-      preprocess: sveltePreprocess()
-   })]
+   clearScreen: false,
+   server: {
+      strictPort: true,
+      host: true
+   },
+   envPrefix: ["VITE_", "TAURI_"],
+   build: {
+      target: ["es2021", "chrome100", "safari13"],
+      // don't minify for debug builds
+      minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+      // produce sourcemaps for debug builds
+      sourcemap: !!process.env.TAURI_DEBUG
+   },
+   plugins: [
+      svelte({
+         preprocess: sveltePreprocess(),
+      }),
+   ]
 });
