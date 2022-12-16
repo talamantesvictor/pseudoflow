49b391a084609d3809df39311278da3773f88f02
Victor Talamantes <victor@binmatter.com>
2022-12-15 17:57:18 -0700

    Fix browser compatibility by removing RegEx lookbehind

--- src/lib/analyzers/lexer.ts
+++ src/lib/analyzers/lexer.ts
@@ -42,7 +42,7 @@ export const lexer = (code: string) : any => {
    // remove comments
    code = code.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g,'');
    // separate words to lexer
-   const regex = /(["'])(?:(?=(\\?))\2.)*?\1|(((?<=\=\s*)|(?<=\(\s*))\-)?(\d?)+\.?\d+|(?:[=&|^+<>/*%!~-]{1,2})|(?:[\\(){}[\];\:\?]|(?:\w+))|[^\s]/g;
+   const regex = /(["'])(?:(?=(\\?))\2.)*?\1|(\-?\d?)+\.?\d+|(?:[=&|^+<>/*%!~-]{1,2})|(?:[\\(){}[\];\:\?]|(?:\w+))|[^\s]/g;
 
    let tokens : Array<atype.Token> = [];
    code.match(regex)?.forEach((word) => {
