f5dd8b026f5a6f2c02fdd8ee3568a31395a6d25e
Victor Talamantes <victor@binmatter.com>
2023-06-06 18:13:44 -0600

    Add App component comments and minor improvements

--- src/App.svelte
+++ src/App.svelte
@@ -11,7 +11,6 @@
    import InformationModal from "./components/modals/InformationModal.svelte";
    import { save } from "@tauri-apps/api/dialog";
    import { invoke } from "@tauri-apps/api/tauri";
-
    import { lexer } from "./lib/analyzers/lexer";
    import { parser } from "./lib/analyzers/parser";
    import { interpreter, interpreterReset, addSentence } from "./lib/code/interpreter";
@@ -29,6 +28,7 @@
    let lastExecutedSentence: atype.SentencesNode;
    let timeoutToParse: any;
 
+   // Shortcut key handling
    window.onkeydown = function (event) {
       clearTimeout(timeoutToParse);
       timeoutToParse = setTimeout(generateTree, 350);
@@ -48,12 +48,22 @@
       }
    }
 
+   // Fix modal title not updating after language change
    translationStore.subscribe((translations) => {
       if (modal) {
          modal.title = translations.APP_SETTINGS_TITLE;
       }
    });
 
+   // Generate tree and perform pre-execution tasks on run button press
+   function prepareExecution() {
+      generateTree()
+      outputText = "<div class=\"hl-comments\">" + $translationStore.APP_PROGRAM_STARTED + " ***</div>";
+      interpreterReset();
+      execute(syntaxTree['body']);
+   }
+
+   // Generate syntax tree for code interpretation and flowchart visualization
    function generateTree() {
       if (pseudocode !== lastPseudocode) {
          lastPseudocode = pseudocode;
@@ -62,13 +72,7 @@
       }
    }
 
-   function prepareExecution() {
-      generateTree()
-      outputText = "<div class=\"hl-comments\">" + $translationStore.APP_PROGRAM_STARTED + " ***</div>";
-      interpreterReset();
-      execute(syntaxTree['body']);
-   }
-
+   // Run program using interpreter, store progress in case of interruption
    function execute(sentences?: atype.SentencesNode[]) {
       const execution = interpreter(sentences);
       outputText += execution.prints;
@@ -81,6 +85,7 @@
       }
    }
 
+   // Insert assignment node into the tree using the value captured from the user
    function capturedMessage(e) {
       const value = e.detail.text? e.detail.text : false;
       enableUserInput = false;
@@ -94,9 +99,9 @@
       execute();
    }
 
-   function importData(e) {
-      let element = document.getElementById("file-import");
-      filename.set(element['value'].split(/(\\|\/)/g).pop());
+   // Import code from a file using an input element in HTML
+   function importDataFromFile(e) {
+      filename.set(e.target.files[0].name);
 
       const reader = new FileReader();
 		reader.addEventListener("load", (event) => {
@@ -108,6 +113,7 @@
 		reader.readAsText(e.target.files[0], "UTF-8");
    }
 
+   // Handle "New Page" button in top bar
    function newButtonClick() {
       if (pseudocode && pseudocode !== savedPseudocode) {
          modal = {
@@ -121,11 +127,13 @@
       }
    }
 
+   // Handle "Open" button in top bar
    function importButtonClick() {
       let element = document.getElementById("file-import");
       element.click();
    }
 
+   // Handle "Save" button in top bar
    async function exportButtonClick() {
       let exported = false;
       // Desktop
@@ -154,6 +162,7 @@
       return exported;
    }
 
+   // Handle "Run" button in top bar
    function runButtonClick(e) {
       if (isProgramRunning = e.detail) {
          prepareExecution();
@@ -164,6 +173,7 @@
       }
    }
 
+   // Handle "Settings" button in top bar
    function settingsButtonClick() {
       modal = {
          title: $translationStore.APP_SETTINGS_TITLE,
@@ -171,6 +181,7 @@
       };
    }
 
+   // Handle "Information" button in top bar
    function infoButtonClick() {
       modal = {
          title: $translationStore.APP_INFO_TITLE,
@@ -178,6 +189,7 @@
       };
    }
 
+   // Used in save-warning-dialog modal
    async function saveAndClose() {
       closeModal();
       if (await exportButtonClick()) {
@@ -185,13 +197,23 @@
       }
    }
 
+   // Used in save-warning-dialog modal
    function closeAndNew() {
       closeModal();
       newDocument();
    }
 
+   // Reset variables
    function newDocument() {
+      isProgramRunning = false;
+      isChartVisible = false;
       pseudocode = '';
+      lastPseudocode = '';
+      savedPseudocode = '';
+      outputText = '';
+      pendingSentencesToExecute = [];
+      lastExecutedSentence = null;
+      clearTimeout(timeoutToParse);
       generateTree();
    }
 
@@ -224,7 +246,7 @@
    </div>
 </div>
 
-<input type="file" id="file-import" on:change={importData} />
+<input type="file" id="file-import" on:change={importDataFromFile} />
 
 {#if modal} 
 <Modal title="{modal.title}" component="{modal.component}" saveDialog="{modal.saveDialog}"
