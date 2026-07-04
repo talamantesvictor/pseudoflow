6f9f78020e597d0d0d002aa3103bfdc8184dda5f
Victor Talamantes <victor@binmatter.com>
2026-07-04 13:09:01 -0600

    feat: add logo and center version row in about modal

--- src/components/modals/InformationModal.svelte
+++ src/components/modals/InformationModal.svelte
@@ -1,11 +1,25 @@
 <script lang="ts">
    import { translationStore } from "../../lib/stores";
    import { version } from '../../../package.json';
+   import logo from '../../../src-tauri/icons/icon-pseudoflow.png';
 </script>
 
-{$translationStore.APP_INFO_VERSION}: {version}<br><br>
+<img src={logo} alt="PseudoFlow" class="logo" />
+<div class="version">{$translationStore.APP_INFO_VERSION}: {version}</div>
 {$translationStore.APP_INFO_DESCRIPTION}<br><br>
 
 <a href="https://www.pseudoflow.app" target="_blank">pseudoflow.app</a>
 
-<style lang="scss"></style>
+<style lang="scss">
+  .logo {
+    display: block;
+    width: 64px;
+    height: 64px;
+    margin: 0 auto 12px auto;
+  }
+
+  .version {
+    text-align: center;
+    margin-bottom: 12px;
+  }
+</style>
