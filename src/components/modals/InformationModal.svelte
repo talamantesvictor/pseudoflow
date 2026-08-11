7e94a7f6baadc1fc581d39e4f73003bf9f411081
Victor Talamantes <victor@binmatter.com>
2026-08-11 17:20:31 -0600

    chore: clean up privacy notice in About modal

--- src/components/modals/InformationModal.svelte
+++ src/components/modals/InformationModal.svelte
@@ -6,12 +6,13 @@
 
 <img src={logo} alt="PseudoFlow" class="logo" />
 <div class="version">{$translationStore.APP_INFO_VERSION}: {version}</div>
-{$translationStore.APP_INFO_DESCRIPTION}<br><br>
+<p class="description">{$translationStore.APP_INFO_DESCRIPTION}</p>
 
-<a href="https://www.pseudoflow.app" target="_blank">pseudoflow.app</a>
-<br><br>
-{$translationStore.APP_INFO_PRIVACY}<br>
-<a href="https://github.com/talamantesvictor/pseudoflow/blob/main/PRIVACY.md" target="_blank">{$translationStore.APP_INFO_PRIVACY_LINK}</a>
+<a class="website" href="https://www.pseudoflow.app" target="_blank">pseudoflow.app</a>
+
+<hr class="separator" />
+
+<p class="privacy">{$translationStore.APP_INFO_PRIVACY}</p>
 
 <style lang="scss">
   .logo {
@@ -24,5 +25,27 @@
   .version {
     text-align: center;
     margin-bottom: 12px;
+  }
+
+  .description {
+    margin: 0 0 8px 0;
+    line-height: 1.5;
+  }
+
+  .website {
+    display: block;
+    margin-bottom: 16px;
+  }
+
+  .separator {
+    border: 0;
+    border-top: 1px solid var(--border-color, rgba(255, 255, 255, 0.1));
+    margin: 16px 0 12px 0;
+  }
+
+  .privacy {
+    margin: 0;
+    font-size: 0.85em;
+    opacity: 0.7;
   }
 </style>
